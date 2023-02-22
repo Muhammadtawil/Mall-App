3fbfeb06fee9fd3b4d858ba454cffbc1179c3f82
Muhammad Tawil <muhammad.tawil@hotmail.com>
2023-02-22 23:10:10 +0200

    add flag

--- ecomall/lib/utilis/theme/app_theme.dart
+++ ecomall/lib/utilis/theme/app_theme.dart
@@ -4,41 +4,42 @@ AppColors colors(context) => Theme.of(context).extension<AppColors>()!;
 
 ThemeData getAppTheme(BuildContext context, bool isDarkTheme) {
   return ThemeData(
-    extensions: <ThemeExtension<AppColors>>[
-      AppColors(
-        color1: isDarkTheme ? Colors.blue : Colors.green,
-        color2: isDarkTheme ? Colors.pink : Colors.blue,
-        color3: isDarkTheme ? Colors.yellow : Colors.red,
-      ),
-    ],
-    scaffoldBackgroundColor: isDarkTheme
-        ? const Color.fromARGB(255, 41, 41, 41)
-        : const Color.fromARGB(255, 230, 230, 230),
-    textTheme: Theme.of(context)
-        .textTheme
-        .copyWith(
-          titleSmall:
-              Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: 12),
-        )
-        .apply(
-          bodyColor: isDarkTheme ? Colors.white : Colors.black,
-          displayColor: Colors.grey,
+      extensions: <ThemeExtension<AppColors>>[
+        AppColors(
+          color1: isDarkTheme ? Colors.blue : Colors.green,
+          color2: isDarkTheme ? Colors.pink : Colors.blue,
+          color3: isDarkTheme ? Colors.yellow : Colors.red,
         ),
-    switchTheme: SwitchThemeData(
-      thumbColor: MaterialStateProperty.all(
-          isDarkTheme ? Colors.orange : Colors.purple),
-    ),
-    listTileTheme: ListTileThemeData(
-        iconColor: isDarkTheme ? Colors.orange : Colors.purple),
-    appBarTheme: AppBarTheme(
-        backgroundColor: isDarkTheme
-            ? const Color.fromARGB(255, 41, 41, 41)
-            : const Color.fromARGB(255, 230, 230, 230),
-        iconTheme: IconThemeData(
-            color: isDarkTheme
-                ? Colors.white
-                : const Color.fromARGB(255, 41, 41, 41))),
-  );
+      ],
+      scaffoldBackgroundColor: isDarkTheme
+          ? const Color.fromARGB(255, 41, 41, 41)
+          : const Color.fromARGB(255, 230, 230, 230),
+      textTheme: Theme.of(context)
+          .textTheme
+          .copyWith(
+            titleSmall:
+                Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: 12),
+          )
+          .apply(
+            bodyColor: isDarkTheme ? Colors.white : Colors.black,
+            displayColor: Colors.grey,
+          ),
+      switchTheme: SwitchThemeData(
+        thumbColor: MaterialStateProperty.all(
+            isDarkTheme ? Colors.orange : Colors.purple),
+      ),
+      listTileTheme: ListTileThemeData(
+          iconColor: isDarkTheme ? Colors.orange : Colors.purple),
+      appBarTheme: AppBarTheme(
+          backgroundColor: isDarkTheme
+              ? const Color.fromARGB(255, 41, 41, 41)
+              : const Color.fromARGB(255, 230, 230, 230),
+          iconTheme: IconThemeData(
+              color: isDarkTheme
+                  ? Colors.white
+                  : const Color.fromARGB(255, 41, 41, 41))),
+      buttonBarTheme: const ButtonBarThemeData(
+          layoutBehavior: ButtonBarLayoutBehavior.constrained));
 }
 
 @immutable
