50a2b8b110fd0bda024d18e4790e7e31e4f406b4
Muhammad Tawil <muhammad.tawil@hotmail.com>
2023-02-25 01:52:36 +0200

    add shop model

--- ecomall/lib/model/shop_model.dart
+++ ecomall/lib/model/shop_model.dart
@@ -1,19 +1,43 @@
 class Shop {
   final String id;
   final String name;
+  final String logoUrl;
   final String description;
   final List items;
   final String imageUrl;
   final String location;
   final double rate;
+  final String categoryType;
+  final String ownerName;
+  final String ownerIdImage;
+  final int follwersCount;
+  final int viewersCount;
+  final int branchesCount;
+  final double profit;
+  final int itemsSelled;
+  bool isclose;
+  bool isBoosted;
+  bool isHaveStore;
 
   Shop(
     this.id,
     this.name,
+    this.logoUrl,
     this.description,
     this.items,
     this.imageUrl,
     this.location,
     this.rate,
+    this.categoryType,
+    this.ownerName,
+    this.ownerIdImage,
+    this.follwersCount,
+    this.viewersCount,
+    this.branchesCount,
+    this.profit,
+    this.itemsSelled,
+    this.isclose,
+    this.isBoosted,
+    this.isHaveStore,
   );
 }
